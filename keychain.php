3ab85a751421c1c521f22905cb47bd953a11f9af
Albin Eriksson <albineriksson100@gmail.com>
2020-08-03 23:13:17 +0200

    Update keychain.php

--- keychain.php
+++ keychain.php
@@ -1,5 +1,5 @@
 <?php
-if($_GET['pass'] == 1234) {
+if($_GET['pass'] == 'ec6a6536ca304edf844d1d248a4f08dc') {
   session_start();
   $_SESSION['keychain'] = True;
   header('location: ../index.html');
