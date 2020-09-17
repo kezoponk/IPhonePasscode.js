c5831261cf420a01077ce68278ed0ceb42011376
Albin Eriksson <albineriksson100@gmail.com>
2020-09-17 14:16:50 +0200

    Update passcode.js

--- passcode.js
+++ passcode.js
@@ -177,7 +177,7 @@ class IPhonePasscode {
     if(this.options.length == countclick) {
       if(this.md5(this.md5(this.enteredPassword)) == this.options.doublemd5password) {
         // Success, a back-end system is required to validate again to avoid exploitation
-        if(this.options.redirect.includes('?') {
+        if(this.options.redirect.includes('?')) {
           window.location = this.options.redirect+"&pass="+this.enteredPassword;
         } else {
           window.location = this.options.redirect+"?pass="+this.enteredPassword;
